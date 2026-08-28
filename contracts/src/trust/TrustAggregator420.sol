182e621cc1f4b46dc2911729b92f893bb53e7970
abvhiael crowley <abvhiael@live.ca>
2026-08-27 18:00:25 -0600

    feat: bind 420Trust aggregator to canonical interface

--- contracts/src/trust/TrustAggregator420.sol
+++ contracts/src/trust/TrustAggregator420.sol
@@ -2,23 +2,15 @@
 pragma solidity ^0.8.24;
 
 import "../interfaces/I420System.sol";
+import "../interfaces/ITrust420.sol";
 import "./TrustSignalRegistry420.sol";
 import "./TrustPolicyRegistry420.sol";
 
-contract TrustAggregator420 is I420System {
+contract TrustAggregator420 is I420System, ITrust420 {
     TrustSignalRegistry420 public immutable signalRegistry;
     TrustPolicyRegistry420 public immutable policyRegistry;
 
     error ZeroAddress();
-
-    struct MetricRead {
-        bytes32 domainId;
-        bytes32 unitId;
-        uint32 metricRevision;
-        bool metricActive;
-        int256 total;
-        uint64 activeSignals;
-    }
 
     constructor(address signalRegistry_, address policyRegistry_) {
         if (signalRegistry_ == address(0) || policyRegistry_ == address(0)) revert ZeroAddress();
@@ -33,11 +25,11 @@ contract TrustAggregator420 is I420System {
     function readMetric(bytes32 subjectType, bytes32 subjectId, bytes32 metricId)
         external
         view
-        returns (MetricRead memory out)
+        returns (ITrust420.MetricRead memory out)
     {
         TrustPolicyRegistry420.Metric memory metric = policyRegistry.getMetric(metricId);
         TrustSignalRegistry420.Aggregate memory aggregate = signalRegistry.getAggregate(subjectType, subjectId, metricId);
-        out = MetricRead({
+        out = ITrust420.MetricRead({
             domainId: metric.domainId,
             unitId: metric.unitId,
             metricRevision: metric.revision,

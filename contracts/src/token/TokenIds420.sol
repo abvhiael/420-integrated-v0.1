c93515ff26bafa530e742e1864d12418fbce6505
abvhiael crowley <abvhiael@live.ca>
2026-09-01 20:54:10 -0600

    fix(token): bind creation fees to community treasury vault

--- contracts/src/token/TokenIds420.sol
+++ contracts/src/token/TokenIds420.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 library TokenIds420 {
     bytes32 internal constant COMPONENT_TOKEN = keccak256("420/component/token/v1");
+    bytes32 internal constant COMMUNITY_TOKEN_REVENUE_VAULT = keccak256("420/treasury/vault/token-creation-community-revenue/v1");
     bytes32 internal constant ERC20_FIXED = keccak256("420/token/template/erc20-fixed/v1");
     bytes32 internal constant ERC20_MINTABLE = keccak256("420/token/template/erc20-mintable/v1");
     bytes32 internal constant ERC20_CAPPED = keccak256("420/token/template/erc20-capped/v1");

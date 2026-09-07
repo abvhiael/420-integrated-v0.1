3e4580644bb6f1d583b26064ed1c5273e22be1c8
abvhiael crowley <abvhiael@live.ca>
2026-09-07 13:26:56 -0600

    build(420wallet): add FCM messaging dependency

--- wallet/mobile/android/app/build.gradle.kts
+++ wallet/mobile/android/app/build.gradle.kts
@@ -39,5 +39,6 @@ dependencies {
     implementation("androidx.credentials:credentials:1.5.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("com.google.firebase:firebase-messaging:24.1.0")
     testImplementation("junit:junit:4.13.2")
 }

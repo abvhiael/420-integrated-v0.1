a9ab8e399401c9f059c842a169c807c6f4e7f67f
abvhiael crowley <abvhiael@live.ca>
2026-09-08 20:19:13 -0600

    feat(gaming-sdk): export public sdk surface

---
+++ packages/420-gaming-sdk/src/index.js
@@ -0,0 +1,8 @@
+export {
+  PlayerAccessState,
+  PromptKind,
+  AccessRequirement,
+  derivePlayerAccessState,
+  evaluateAccessRequirement
+} from "./access-state.js";
+export { createGamingClient420 } from "./client.js";

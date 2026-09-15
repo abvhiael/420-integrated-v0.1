932d40396b95139ef71c59964da3c145b4953a1d
abvhiael crowley <abvhiael@live.ca>
2026-09-15 13:35:11 -0600

    SR-7.10 qualify node gateway retry configuration

--- execution/cmd/node420/gateway_service_test.go
+++ execution/cmd/node420/gateway_service_test.go
@@ -8,14 +8,44 @@ import (
 	"time"
 )
 
+func preserveGatewayRetryFlags(t *testing.T) func() {
+	t.Helper()
+	oldAttempts, oldBackoff, oldMaxBackoff := *gatewayUpstreamAttempts, *gatewayUpstreamBackoff, *gatewayUpstreamMaxBackoff
+	return func() {
+		*gatewayUpstreamAttempts = oldAttempts
+		*gatewayUpstreamBackoff = oldBackoff
+		*gatewayUpstreamMaxBackoff = oldMaxBackoff
+	}
+}
+
 func TestNewNodeGatewayServiceRequiresUpstream(t *testing.T) {
 	oldListen, oldCache, oldStore, oldTimeout := *gatewayListen, *gatewayCacheURL, *gatewayStoreURL, *gatewayTimeout
 	defer func() { *gatewayListen, *gatewayCacheURL, *gatewayStoreURL, *gatewayTimeout = oldListen, oldCache, oldStore, oldTimeout }()
+	defer preserveGatewayRetryFlags(t)()
 	*gatewayListen = "127.0.0.1:0"
 	*gatewayCacheURL = ""
 	*gatewayStoreURL = ""
 	*gatewayTimeout = time.Second
+	*gatewayUpstreamAttempts = 3
+	*gatewayUpstreamBackoff = time.Millisecond
+	*gatewayUpstreamMaxBackoff = 10 * time.Millisecond
 	if _, err := newNodeGatewayService(); err == nil { t.Fatal("expected missing upstream error") }
+}
+
+func TestNewNodeGatewayServiceRejectsInvalidRetryPolicy(t *testing.T) {
+	upstream := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadGateway)
+	}))
+	defer upstream.Close()
+	oldListen, oldCache, oldStore, oldTimeout := *gatewayListen, *gatewayCacheURL, *gatewayStoreURL, *gatewayTimeout
+	defer func() { *gatewayListen, *gatewayCacheURL, *gatewayStoreURL, *gatewayTimeout = oldListen, oldCache, oldStore, oldTimeout }()
+	defer preserveGatewayRetryFlags(t)()
+	*gatewayListen = "127.0.0.1:0"
+	*gatewayCacheURL = upstream.URL
+	*gatewayStoreURL = ""
+	*gatewayTimeout = time.Second
+	*gatewayUpstreamAttempts = 0
+	if _, err := newNodeGatewayService(); err == nil { t.Fatal("expected invalid retry policy error") }
 }
 
 func TestNewNodeGatewayServiceLifecycle(t *testing.T) {
@@ -26,10 +56,14 @@ func TestNewNodeGatewayServiceLifecycle(t *testing.T) {
 
 	oldListen, oldCache, oldStore, oldTimeout := *gatewayListen, *gatewayCacheURL, *gatewayStoreURL, *gatewayTimeout
 	defer func() { *gatewayListen, *gatewayCacheURL, *gatewayStoreURL, *gatewayTimeout = oldListen, oldCache, oldStore, oldTimeout }()
+	defer preserveGatewayRetryFlags(t)()
 	*gatewayListen = "127.0.0.1:0"
 	*gatewayCacheURL = upstream.URL
 	*gatewayStoreURL = ""
 	*gatewayTimeout = time.Second
+	*gatewayUpstreamAttempts = 3
+	*gatewayUpstreamBackoff = time.Millisecond
+	*gatewayUpstreamMaxBackoff = 10 * time.Millisecond
 
 	service, err := newNodeGatewayService()
 	if err != nil { t.Fatal(err) }

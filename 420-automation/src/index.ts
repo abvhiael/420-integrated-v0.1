592bf71004955e819deb75e3a693a16ce882477c
abvhiael crowley <abvhiael@live.ca>
2026-09-15 12:08:21 -0600

    feat(gas): export Automation sponsorship preparation flow

--- 420-automation/src/index.ts
+++ 420-automation/src/index.ts
@@ -6,6 +6,7 @@ export * from './execution.js';
 export * from './funding.js';
 export * from './gas-integration.js';
 export * from './gas-quote-adapter.js';
+export * from './gas-sponsorship-flow.js';
 export * from './recovery.js';
 export * from './workers.js';
 export * from './oracle.js';
